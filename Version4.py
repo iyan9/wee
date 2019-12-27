014efa07bdc265bf1e0fcc8e4761c8e0a2bc6e43
matty106 <58811269+matty106@users.noreply.github.com>
2019-12-27 16:35:03 +0800

    Create Version4.py

---
+++ Version4.py
@@ -0,0 +1,24 @@
+import pygame
+from pygame.locals import MOUSEBUTTONDOWN, QUIT
+
+switchIcon = pygame.image.load("C:\\Users\\Matty\\Desktop\\橘block.png")
+switchIconrect = switchIcon.get_rect()
+
+# 按鈕放置位置
+x = 300
+y = 410
+
+# 按鈕大小
+IMAGEWIDTH = 50
+IMAGEHEIGHT = 50
+
+# if xiaoxiaole.delete_cnt() >= 9:
+screen.blit(switchIcon, (x, y))
+pygame.display.update()
+
+
+while True:
+    event = pygame.event.poll()
+    if event.type == MOUSEBUTTONDOWN:
+        if x - 4 < pygame.mouse.get_pos()[0] < x - 4 + IMAGEWIDTH and x - 4 < pygame.mouse.get_pos()[1] < x - 4 + IMAGEHEIGHT:
+            效果
